266658231ee3c9b65154069b5d19c13b13d19675
Kieran Edge <kesedge94@gmail.com>
2025-12-03 13:18:59 +0000

    Day 3 data processing added

---
+++ Day3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Day3.Services;
+
+string filePath = Path.Combine(AppContext.BaseDirectory, "Data.txt");
+
+List<List<int>> batteryRows = DataProcessor.TextFileToListOfListOfInts(filePath);
+
+foreach (List<int> batteryRow in batteryRows)
+{
+    foreach(int battery in batteryRow)
+    {
+        Console.WriteLine(battery);
+    }
+}
---
+++ Day3/Services/DataProcessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3.Services
+{
+    public static class DataProcessor
+    {
+        public static List<List<int>> TextFileToListOfListOfInts(string filePath)
+        {
+            List<List<int>> batteryRows = new List<List<int>>();
+            
+            List<string> fileLines = File.ReadLines(filePath).ToList();
+
+            foreach (string line in fileLines)
+            {
+                List<int> batteryRow = new List<int>();
+                foreach (char c in line)
+                {
+
+                    string batteryValueStr = Convert.ToString(c);
+                    int batteryValue = Convert.ToInt32(batteryValueStr);
+                    batteryRow.Add(batteryValue);
+                }
+                batteryRows.Add(batteryRow);
+            }
+
+            return batteryRows;
+        }
+    }
+}
