0a93342ce3c3e63a3fe9aebbcee5467b38bda413
Kieran Edge <kesedge94@gmail.com>
2025-12-03 19:40:01 +0000

    Day 3 Part 2 Complete

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,13 +1,22 @@
 ﻿using Day3.Services;
 
 string filePath = Path.Combine(AppContext.BaseDirectory, "Data.txt");
+bool isPart1 = false;
 
 List<List<int>> batteryRows = DataProcessor.TextFileToListOfListOfInts(filePath);
-List<int> maxJoltages = new List<int>();
+List<long> maxJoltages = new List<long>();
 
 foreach (var batteryRow in batteryRows)
 {
-    maxJoltages.Add(RowAnalyser.MaxJoltageCalculator(batteryRow));
+    if (isPart1)
+    {
+        maxJoltages.Add(RowAnalyser.MaxJoltageCalculatorPart1(batteryRow));
+        Console.WriteLine($"Sum of max joltages Part 1= {maxJoltages.Sum()}");
+    }
+    else
+    {
+        maxJoltages.Add(RowAnalyser.MaxJoltageCalculatorPart2(batteryRow));
+    }   
 }
 
-Console.WriteLine($"Sum of max joltages = {maxJoltages.Sum()}");
+Console.WriteLine($"Sum of max joltages Part 2 = {maxJoltages.Sum()}");
--- Day3/Services/RowAnalyser.cs
+++ Day3/Services/RowAnalyser.cs
@@ -8,7 +8,7 @@ namespace Day3.Services
 {
     public static class RowAnalyser
     {
-        public static int MaxJoltageCalculator(List<int> joltages)
+        public static int MaxJoltageCalculatorPart1(List<int> joltages)
         {
             // Starting the variables
             int maxJoltageStart = 0;
@@ -36,12 +36,64 @@ namespace Day3.Services
 
             // Concatenating the values and returning
             string maxJoltageString = maxJoltageStart.ToString() + maxJoltageEnd.ToString();
-            //Console.WriteLine($"Max joltage first digit: {maxJoltageStart}");
-            //Console.WriteLine($"Max joltage second digit: {maxJoltageEnd}");
-            //Console.WriteLine($"Max joltage: {maxJoltageString}");
-            //Thread.Sleep(1000);
 
             return Convert.ToInt32(maxJoltageString);
+        }
+
+        public static long MaxJoltageCalculatorPart2(List<int> joltages)
+        {
+            // Starting the variables
+            int maxJoltageInRow = 0;
+            int maxJoltageStartingIndex = 0;
+            int stringLengthModifier = 11;
+            string concatenatedValues = "";
+            List<int> maxJoltageValues = new List<int>();
+
+            // Populating the 12 values
+            for(int j = 0; j < 12; ++j)
+            { 
+                // Getting the largest digit and its index
+                List<int> tempDigitList = FindNextLongestNumber(maxJoltageStartingIndex, stringLengthModifier, joltages);
+
+                // Adding to the list
+                maxJoltageValues.Add(tempDigitList[0]);
+
+                // Updating the search parameters
+                maxJoltageStartingIndex = tempDigitList[1] + 1;
+                stringLengthModifier--;
+                
+            }
+
+            foreach(int digit in maxJoltageValues)
+            {
+                concatenatedValues += digit.ToString();
+            }
+            
+
+            return (long)Convert.ToDouble(concatenatedValues);
+        }
+
+        public static List<int> FindNextLongestNumber(int startingIndex, int stringLengthModifier, List<int> joltages)
+        {
+            int maxDigit = 0;
+            int maxDigitIndex = 0;
+            List<int> maxDigitInformation = new List<int>();
+            
+            // Scanning the values to get the max numbers
+            for (int i = startingIndex; i < joltages.Count - stringLengthModifier; i++)
+            {
+                if (joltages[i] > maxDigit)
+                {
+                    maxDigit = joltages[i];
+                    maxDigitIndex = i;
+                }
+            }
+
+            // Returning the max digit and its index as a list
+            maxDigitInformation.Add(maxDigit);
+            maxDigitInformation.Add(maxDigitIndex);
+            return maxDigitInformation;
+            
         }
     }
 }
