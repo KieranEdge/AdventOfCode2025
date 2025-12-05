c5ccfa093ac1f2adabdd63848825a9e5e1bbec43
Kieran Edge <kesedge94@gmail.com>
2025-12-05 13:54:20 +0000

    Day 5 Part 1

---
+++ Day5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Day5.Services;
+
+// File Paths
+string rangeFilePath = Path.Combine(AppContext.BaseDirectory, "RangeData.txt");
+string ingredientsFilePath = Path.Combine(AppContext.BaseDirectory, "IngredientData.txt");
+
+// Extracting the data from the string
+List<List<long>> acceptableIngredients = DataExtractor.RangeDataExtractor(rangeFilePath);
+List<long> ingredients = DataExtractor.ListOfIngredientsAsLongs(ingredientsFilePath);
+int numberOfRanges = acceptableIngredients[0].Count;
+
+// Finding the number of acceptable ingredients
+int countOfAcceptableIngredients = 0;
+
+foreach(long ingredient in ingredients)
+{
+    bool ingredientFound = false;
+    
+    for (int i = 0; i < numberOfRanges; i++)
+    {
+        if (ingredientFound)
+        {
+            continue;
+        }
+        else
+        {
+            if (ingredient >= acceptableIngredients[0][i] && ingredient <= acceptableIngredients[1][i])
+            {
+                countOfAcceptableIngredients++;
+                ingredientFound = true;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Number of acceptable ingredients = {countOfAcceptableIngredients}");
---
+++ Day5/Services/DataExtractor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5.Services
+{
+    internal static class DataExtractor
+    {
+        public static List<List<long>> RangeDataExtractor(string filePath)
+        {
+            // Getting the data from the text file
+            string[] listOfRanges = File.ReadAllLines(filePath);
+
+            // Converting to longs and adding to a list
+            List<long> ingredientsInBottomOfRange = new List<long>();
+            List<long> ingredientsInTopOfRange = new List<long>();
+            List<List<long>> combinedRanges = new List<List<long>>();
+
+            foreach (string range in listOfRanges)
+            {
+                long rangeStart = long.Parse(range.Split('-')[0]);
+                long rangeEnd = long.Parse(range.Split('-')[1]);
+
+                ingredientsInBottomOfRange.Add(rangeStart);
+                ingredientsInTopOfRange.Add(rangeEnd);
+            }
+
+            combinedRanges.Add(ingredientsInBottomOfRange);
+            combinedRanges.Add(ingredientsInTopOfRange);
+
+            return combinedRanges;
+
+        }
+
+        public static List<long> ListOfIngredientsAsLongs(string filePath)
+        {
+            // Getting all of the ingredients as an array
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Iterating over them and returning them as a list
+            List<long> ingredients = new List<long>();
+            foreach (string line in lines)
+            {
+                ingredients.Add(long.Parse(line));
+            }
+            return ingredients;
+        }
+    }
+}
